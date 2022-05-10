c093917684e8bc8449d34d9b6ccd80cbe0781b6a
Prashant Rawat <pr7prashant@gmail.com>
2022-05-10 08:31:30 +0530

    Time: 339 ms (36.11%), Space: 97 MB (83.70%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
@@ -6,34 +6,24 @@ private:
 public:
     RandomizedSet() {}
     
-    void print() {
-        for (int n : rset) cout << n << " , ";
-        cout << endl;
-    }
-    
     bool insert(int val) {
-        // cout << "Add " << val << endl;
         if (mp.find(val) != mp.end()) return false;
         rset.push_back(val);
         mp[val] = rset.size() - 1;
-        // print();
         return true;
     }
     
     bool remove(int val) {
-        // cout << "Remove " << val << endl;
         if (mp.find(val) == mp.end()) return false;
         int idx = mp[val];
         mp[rset.back()] = idx;
         rset[idx] = rset.back();
         rset.pop_back();
         mp.erase(val);
-        // print();
         return true;
     }
     
     int getRandom() {
-        // cout << "Get Random " << endl;
         return rset[rand() % rset.size()];
     }
 };
