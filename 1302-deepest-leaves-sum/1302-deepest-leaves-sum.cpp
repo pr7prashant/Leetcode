e89c9f88afcaaf15a7a51addb47015f41ea8848c
Prashant Rawat <pr7prashant@gmail.com>
2022-05-15 08:23:29 +0530

    Time: 172 ms (18.22%), Space: 61.9 MB (27.92%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,36 +11,10 @@
  */
 class Solution {
 public:
-    int max_depth;
-    int sum;
-    
-    void traverse(TreeNode* root, int depth) {
-        if (!root) return;
-        
-        if (!root->left && !root->right) {
-            if (depth > max_depth) {
-                max_depth = depth;
-                sum = root->val;
-            } else if (depth == max_depth) {
-                sum += root->val;
-            }
-            return;
-        }
-        
-        traverse(root->left, depth + 1);
-        traverse(root->right, depth + 1);
-    }
-    
     int deepestLeavesSum(TreeNode* root) {
-        // max_depth = 0;
-        // sum = 0;
-        // traverse(root, 0);
-        // return sum;
-        
         int sum = 0;
         queue<TreeNode*> q;
         q.push(root);
-        
         while (!q.empty()) {
             int s = q.size();
             sum = 0;
