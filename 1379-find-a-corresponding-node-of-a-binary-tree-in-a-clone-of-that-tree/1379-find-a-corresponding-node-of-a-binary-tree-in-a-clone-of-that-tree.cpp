efc16688c2130f2cd25f39d4a5fe5a76cc63ebde
Prashant Rawat <pr7prashant@gmail.com>
2022-05-17 09:33:45 +0530

    Time: 697 ms (52.92%), Space: 164 MB (22.94%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -10,35 +10,7 @@
 
 class Solution {
 public:
-    vector<int> path;
-    
-    bool getPath(TreeNode* root, TreeNode* target) {
-        if (!root) return false;
-        if (root == target) return true;
-        
-        path.push_back(-1);
-        if (getPath(root->left, target)) return true;
-        path.pop_back();
-        
-        path.push_back(1);
-        if (getPath(root->right, target)) return true;
-        path.pop_back();
-        
-        return false;
-    }
-    
-    TreeNode* getClone(TreeNode* root, int i) {
-        if (i == path.size()) return root;
-        if (path[i] == -1) return getClone(root->left, i+1);
-        if (path[i] == 1) return getClone(root->right, i+1);
-        return root;
-    }
-    
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        // bool found = getPath(original, target);
-        // if (!found) return nullptr;
-        // return getClone(cloned, 0);
-        
         if (original == nullptr || original == target) return cloned;
         TreeNode* res = getTargetCopy(original->left, cloned->left, target);
         if (res) return res;
