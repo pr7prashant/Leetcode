017139171bf1316497c2177041545e3e63a7d734
Prashant Rawat <pr7prashant@gmail.com>
2022-05-22 08:04:06 +0530

    Time: 31 ms (25.60%), Space: 17.2 MB (41.10%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -2,33 +2,22 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> ans;
-        bool found = false;
+        int i = 0;
         
-        for (vector<int>& interval : intervals) {
-            if (
-                !found && 
-                (
-                    newInterval[0] >= interval[0] && newInterval[0] <= interval[1] ||
-                    newInterval[1] >= interval[0] && newInterval[1] <= interval[1] ||
-                    newInterval[0] <= interval[0] && newInterval[1] >= interval[1]
-                )
-            ) {
-                ans.push_back({ min(interval[0], newInterval[0]), max(interval[1], newInterval[1]) });
-                found = true;
-            } else if (!found && newInterval[1] < interval[0]) {
-                ans.push_back(newInterval);
-                ans.push_back(interval);
-                found = true;
-            } else if (ans.size() > 0 && interval[0] <= ans.back()[1]) {
-                vector<int> temp = ans.back();
-                ans.pop_back();
-                ans.push_back({ min(interval[0], temp[0]), max(interval[1], temp[1]) });
-            } else {
-                ans.push_back(interval);
-            }
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
+            ans.push_back(intervals[i++]);
         }
         
-        if (!found) ans.push_back(newInterval);
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            newInterval = { min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1]) };
+            i++;
+        }
+        
+        ans.push_back(newInterval);
+        
+        while (i < intervals.size() ) {
+            ans.push_back(intervals[i++]);
+        }
         
         return ans;
     }
