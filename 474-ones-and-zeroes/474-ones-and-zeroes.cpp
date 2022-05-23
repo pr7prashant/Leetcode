12bee4140d22b68d8d2c7a68d315e9cd05ac635c
Prashant Rawat <pr7prashant@gmail.com>
2022-05-23 07:43:47 +0530

    Time: 1916 ms (5.03%), Space: 229.2 MB (5.76%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
-    // unordered_map<string, vector<int>> mp;
-    
     vector<int> canPick(string& s, int m, int n) {
-        // string key = s + "_" + to_string(m) + "_" + to_string(n);
-        // if (mp.find(key) != mp.end()) return mp[key];
-        
         if (s.length() > m + n) return {0, 0, 0};
 
         int zeros = 0, ones = 0;
@@ -24,7 +19,6 @@ public:
         if (dp[m][n][i] != -1) return dp[m][n][i];
         
         vector<int> res = canPick(strs[i-1], m, n);
-        // cout << m << " , " << n << " , " << i << ", {" << res[0] << "}" << endl;
         if (res[0] == 1) {
             return dp[m][n][i] = max(1 + helper(strs, m-res[1], n-res[2], i-1, dp), helper(strs, m, n, i-1, dp));
         }
@@ -34,7 +28,6 @@ public:
     
     int findMaxForm(vector<string>& strs, int m, int n) {
         vector<vector<vector<int>>> dp(m+1, vector<vector<int>>(n+1, vector<int>(strs.size() + 1, -1)));
-        // cout << "test : " << canPick(strs[2], 1, 2)[0] << endl;
         return helper(strs, m, n, strs.size(), dp);
     }
 };
