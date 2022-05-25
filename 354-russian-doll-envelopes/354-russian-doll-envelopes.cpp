df17ec6656ebcfbb858433e3b5bc5346e23e7ae8
Prashant Rawat <pr7prashant@gmail.com>
2022-05-25 08:53:12 +0530

    Time: 429 ms (69.63%), Space: 77.6 MB (44.39%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -10,11 +10,24 @@ public:
             if (temp.empty() || temp.back() < e[1]) {
                 temp.push_back(e[1]);
             } else {
-                int idx = lower_bound(temp.begin(), temp.end(), e[1]) - temp.begin();
-                temp[idx] = e[1];
+                // int idx = lower_bound(temp.begin(), temp.end(), e[1]) - temp.begin();
+                // temp[idx] = e[1];
+                
+                int l = 0, r = temp.size() - 1;
+                while (l < r) {
+                    int mid = (l + r) / 2;
+                    if (e[1] > temp[mid]) {
+                        l = mid + 1;
+                    } else {
+                        r = mid;
+                    }
+                }
+                temp[l] = e[1];
             }
         }
         
         return temp.size();
     }
 };
+
+// 3,4,4,7
