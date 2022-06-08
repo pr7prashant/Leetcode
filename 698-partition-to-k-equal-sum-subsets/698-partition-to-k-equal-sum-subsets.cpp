086c9fe15f525b5d76714f795eea88fb4924671a
Prashant Rawat <pr7prashant@gmail.com>
2022-06-08 08:11:16 +0530

    Time: 1654 ms (8.43%), Space: 9.5 MB (48.16%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -23,16 +23,13 @@ public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int sum = 0;
         for (int n : nums) sum += n;
-        
         if (sum % k != 0) return false;
         
-        int s = nums.size();
-        bool visited[s];
+        int n = nums.size();
+        bool visited[n];
         int subsetSum[k];
         for (int i = 0; i < k; i++) subsetSum[i] = 0;
-        for (int i = 0; i < s; i++) visited[i] = false;
-        // vector<bool> visited(nums.size(), false);
-        // vector<int> subsetSum(k, 0);
+        for (int i = 0; i < n; i++) visited[i] = false;
         
         return solve(nums, subsetSum, visited, k, sum/k, 0, 0);
     }
