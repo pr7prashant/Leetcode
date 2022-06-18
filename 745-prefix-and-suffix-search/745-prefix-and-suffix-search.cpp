946248120b0dc1ea5aa58873abc91045e269215f
Prashant Rawat <pr7prashant@gmail.com>
2022-06-18 12:54:38 +0530

    Time: 1065 ms (18.29%), Space: 200.1 MB (21.27%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -13,7 +13,6 @@ public:
     };
     
     void insert(TrieNode* root, string& word, int p) {
-        // cout << word << " : " << p << endl;
         TrieNode* curr = root;
         for (int i = 0; i < word.length(); i++) {
             int idx = word[i] - 'a';
@@ -21,7 +20,6 @@ public:
                 curr->next[idx] = new TrieNode(word[i]);
             }
             curr = curr->next[idx];
-            // cout << idx << " : " << curr->ch << endl;
         }
         curr->pos = p;
     }
@@ -35,18 +33,15 @@ public:
             string word = words[i];
             for (int j = 0; j <= word.length(); j++) {
                 string str = word.substr(word.length()-j, j) + "{" + word;
-                // cout << str << endl;
                 insert(root, str, i);
             }
         }
     }
     
     TrieNode* find(TrieNode* root, string& str) {
-        // cout << str << endl;
         TrieNode* curr = root;
         for (int i = 0; i < str.length(); i++) {
             int idx = str[i] - 'a';
-            // cout << idx << " : " << curr->ch << endl;
             if (!curr->next[idx]) return nullptr;
             curr = curr->next[idx];            
         }
@@ -55,7 +50,6 @@ public:
     
     int findMax(TrieNode* root) {
         if (!root) return -1;
-        // cout << root->ch << " : " << root->pos << endl;
         int ans = root->pos;
         for (int i = 0; i < 27; i++) {
             ans = max(ans, findMax(root->next[i]));
@@ -65,7 +59,6 @@ public:
     
     int f(string prefix, string suffix) {
         string str = suffix + "{" + prefix;
-        // cout << str << endl;
         TrieNode* n = find(root, str);
         if (!n) return -1;
         return findMax(n);
