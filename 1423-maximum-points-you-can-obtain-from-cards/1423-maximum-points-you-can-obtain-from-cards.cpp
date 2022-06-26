039fbf4af30a7e0272c6c026ba42b7957e6d4171
Prashant Rawat <pr7prashant@gmail.com>
2022-06-26 07:55:21 +0530

    Time: 84 ms (55.63%), Space: 46.1 MB (8.03%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
-//     unordered_map<string,int> dp;
-    
-//     int solve(vector<int>& cardPoints, int k, int start, int end) {
-//         if (k == 0) return 0;
-        
-//         string key = to_string(start) + "_" + to_string(end) + "_" + to_string(k);
-//         if (dp.find(key) != dp.end()) return dp[key];
-        
-//         return dp[key] = max(
-//             cardPoints[start] + solve(cardPoints, k-1, start+1, end),
-//             cardPoints[end] + solve(cardPoints, k-1, start, end-1)
-//         );
-//     }
-    
     int maxScore(vector<int>& cardPoints, int k) {
-        // return solve(cardPoints, k, 0, cardPoints.size() - 1);
         int len = cardPoints.size();
         vector<int> presum(cardPoints.begin(), cardPoints.end());
         presum.insert(presum.begin(), 0);
