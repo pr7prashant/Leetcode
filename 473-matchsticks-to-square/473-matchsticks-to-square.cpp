f9cd4b65e3b8bc047c6778cb499afb1d57aae4d4
Prashant Rawat <pr7prashant@gmail.com>
2022-07-12 08:14:19 +0530

    Time: 730 ms (29.43%), Space: 10.2 MB (47.49%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool solve(vector<int>& matchsticks, vector<int>& square, int side, int curr) {
+        if (curr == matchsticks.size()) {
+            for (int i = 0; i < 4; i++) {
+                if (square[i] != side) return false;
+            }
+            return true;
+        }
+        
+        if (matchsticks[curr] > side) return false;
+        
+        for (int i = 0; i < 4; i++) {
+            if (matchsticks[curr] <= side - square[i]) {
+                int temp = square[i];
+                square[i] += matchsticks[curr];
+                if (solve(matchsticks, square, side, curr + 1)) return true;
+                square[i] = temp;
+            }
+        }
+        
+        return false;
+    }
+    
+    bool makesquare(vector<int>& matchsticks) {
+        int total = 0;
+        for (int len : matchsticks) total += len;
+        if (total % 4 != 0) return false;
+        
+        sort(matchsticks.begin(), matchsticks.end(), greater<int>());
+        vector<int> square(4, 0);
+        
+        // for (int len : square) cout << len << " , ";
+        
+        return solve(matchsticks, square, total / 4, 0);
+    }
+};
