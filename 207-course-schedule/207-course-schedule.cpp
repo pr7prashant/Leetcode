a4791d307a667face9dc78c84213012f138df62a
Prashant Rawat <pr7prashant@gmail.com>
2022-07-25 10:04:56 +0530

    Time: 32 ms (57.60%), Space: 14.8 MB (23.03%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool hasCycle(unordered_map<int, vector<int>>& graph, int start, vector<bool>& visited, vector<bool>& path) {
+        visited[start] = path[start] = true;
+        for (int n : graph[start]) {
+            if (!visited[n]) {
+                if (hasCycle(graph, n, visited, path)) return true;
+            } else if (path[n]) {
+                return true;
+            }
+        }
+        path[start] = false;
+        
+        return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // Create Graph
+        unordered_map<int, vector<int>> graph;
+        for (vector<int>& curr : prerequisites) graph[curr[1]].push_back(curr[0]);
+        
+        // Check Cycle
+        vector<bool> visited(numCourses, false);
+        vector<bool> path(numCourses, false);
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i] && hasCycle(graph, i, visited, path)) return false;
+        }
+        
+        return true;
+    }
+};
