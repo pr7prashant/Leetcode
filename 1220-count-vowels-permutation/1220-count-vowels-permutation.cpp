be125ae0bae345fd62181c55a5d45867e677d4ae
Prashant Rawat <pr7prashant@gmail.com>
2022-08-07 10:55:37 +0530

    Time: 156 ms (29.44%), Space: 53.3 MB (13.71%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     long long MOD = 1e9 + 7;
-    unordered_map<string,long long> dp;
     
-    long long helper(int n, char ch) {
+    long long helper(int n, char ch, vector<vector<int>>& dp) {
         if (n == 0) return 1;
         
-        string key = to_string(n) + "_" + ch;
-        if (dp.count(key)) return dp[key];
+        if (dp[n][ch - 'a'] != -1) return dp[n][ch - 'a'];
         
         long long ans = 0;
-        if (ch == 'a') ans += helper(n-1, 'e');
-        else if (ch == 'e') ans += helper(n-1, 'a') + helper(n-1, 'i');
-        else if (ch == 'i') ans += helper(n-1, 'a') + helper(n-1, 'e') + helper(n-1, 'o') + helper(n-1, 'u');
-        else if (ch == 'o') ans += helper(n-1, 'i') + helper(n-1, 'u');
-        else if (ch == 'u') ans += helper(n-1, 'a');
+        if (ch == 'a') ans += helper(n-1, 'e', dp);
+        else if (ch == 'e') ans += helper(n-1, 'a', dp) + helper(n-1, 'i', dp);
+        else if (ch == 'i') ans += helper(n-1, 'a', dp) + helper(n-1, 'e', dp) + helper(n-1, 'o', dp) + helper(n-1, 'u', dp);
+        else if (ch == 'o') ans += helper(n-1, 'i', dp) + helper(n-1, 'u', dp);
+        else if (ch == 'u') ans += helper(n-1, 'a', dp);
         
-        return dp[key] = ans % MOD;
+        return dp[n][ch - 'a'] = ans % MOD;
     }
     
     int countVowelPermutation(int n) {
         vector<char> arr = { 'a', 'e', 'i', 'o', 'u' };
+        vector<vector<int>> dp(n+1, vector<int>(26, -1));
         long long ans = 0;
-        for (char ch : arr) ans += helper(n-1, ch);
+        for (char ch : arr) ans += helper(n-1, ch, dp);
         return ans % MOD;
     }
 };
