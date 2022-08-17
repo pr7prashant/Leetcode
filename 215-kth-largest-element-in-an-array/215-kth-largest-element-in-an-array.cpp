f91ce972a734b908db08077919a26ed3d88a7f33
Prashant Rawat <pr7prashant@gmail.com>
2022-08-17 09:44:40 +0530

    Time: 144 ms (25.18%), Space: 47.3 MB (13.93%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -4,7 +4,7 @@ public:
         priority_queue<int, vector<int>, greater<int>> pq;
         
         for (int n : nums) {
-            if (pq.size() < k || n > pq.top()) pq.push(n);
+            pq.push(n);
             if (pq.size() > k) pq.pop();
         }
         
