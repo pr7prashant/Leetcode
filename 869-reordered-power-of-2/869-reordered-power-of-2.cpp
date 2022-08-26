13dde4e53d0f103108b3567ef8fabe8e1f5876b2
Prashant Rawat <pr7prashant@gmail.com>
2022-08-26 09:21:48 +0530

    Time: 5 ms (53.95%), Space: 6 MB (83.90%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    bool isPowerOf2(int n) {
-        int p = 1;
-        while (p < n) p <<= 1;
-        return p == n;
-    }
+//     bool isPowerOf2(int n) {
+//         int p = 1;
+//         while (p < n) p <<= 1;
+//         return p == n;
+//     }
     
-    bool helper(string s, int start) {
-        if (isPowerOf2(stoi(s))) return true;
+//     bool helper(string s, int start) {
+//         if (isPowerOf2(stoi(s))) return true;
         
-        bool ans = false;
-        for (int i = start; i < s.length(); i++) {
-            if (start == 0 && s[i] == '0') continue;
-            swap(s[start], s[i]);
-            ans |= helper(s, start + 1);
-            swap(s[start], s[i]);
-        }
+//         bool ans = false;
+//         for (int i = start; i < s.length(); i++) {
+//             if (start == 0 && s[i] == '0') continue;
+//             swap(s[start], s[i]);
+//             ans |= helper(s, start + 1);
+//             swap(s[start], s[i]);
+//         }
         
-        return ans;
-    }
+//         return ans;
+//     }
     
     bool reorderedPowerOf2(int n) {
         string num = to_string(n);
-        return helper(num, 0);
+        sort(num.begin(), num.end());
+        for (int i = 0; i < 32; i++) {
+            int p = 1 << i;
+            string temp = to_string(p);
+            sort(temp.begin(), temp.end());
+            if (num == temp) return true;
+        }
+        return false;
     }
 };
