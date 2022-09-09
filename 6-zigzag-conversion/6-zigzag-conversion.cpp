2829b2d4264dc789fe421d7def46946ff278a2aa
Prashant Rawat <pr7prashant@gmail.com>
2022-09-09 10:03:06 +0530

    Time: 166 ms (6.16%), Space: 39.8 MB (10.65%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        
+        vector<vector<char>> mat(numRows, vector<char>(s.length(), '#'));
+        int x = 0, y = 0, i = 0, dir = 0;
+        while (i < s.length()) {
+            while (x < numRows && i < s.length()) mat[x++][y] = s[i++];
+            dir = 1;
+            x = numRows - 2;
+            y++;
+            while (x >= 0 && i < s.length()) mat[x--][y++] = s[i++];
+            dir = 0;
+            x = 1;
+            y--;
+        }
+        
+        string ans = "";
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[0].size(); j++) {
+                if (mat[i][j] != '#') ans += mat[i][j];
+                // cout << mat[i][j] << " ";
+            }
+            // cout << endl;
+        }
+        
+        return ans;
+    }
+};
