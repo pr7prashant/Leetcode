54e03ba80b0f1aafe2c9072fede2341b9ff65b2c
Prashant Rawat <pr7prashant@gmail.com>
2022-09-09 10:25:06 +0530

    Time: 6 ms (25.20%), Space: 6.2 MB (5.43%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -7,14 +7,11 @@ public:
         x = abs(x);
         
         while (pow(10,p) <= x) p++;
-        // cout << p << endl;
         while (x > 0) {
             int d = x % 10;
-            // cout << d << " ";
             x /= 10;
             rev += d * pow(10, --p);
             if (rev > INT_MAX) return 0;
-            // cout << rev << endl;
         }
         
         if (isNegative) rev *= -1;
