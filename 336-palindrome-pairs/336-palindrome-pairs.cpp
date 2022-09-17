105736a894928b290de0998c74409c9a27edaa8b
Prashant Rawat <pr7prashant@gmail.com>
2022-09-17 18:13:25 +0530

    Time: 1698 ms (51.57%), Space: 487.4 MB (45.84%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.cpp
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
+    bool isPalindrome(string& str, int start, int end) {
+        while (start < end) {
+            if (str[start++] != str[end--]) return false;
+        }
+        return true;
+    }
+
     vector<vector<int>> palindromePairs(vector<string>& words) {
         vector<vector<int>> ans;
-        unordered_map<string, int> wmap;
-        
-        for (int i = 0; i < words.size(); i++)
-            wmap[words[i]] = i;
+        unordered_map<string,int> mp;
+        for (int i = 0; i < words.size(); i++) mp[words[i]] = i;
         
         for (int i = 0; i < words.size(); i++) {
+            // 1. Empty string : check if other string is palindrome
             if (words[i] == "") {
                 for (int j = 0; j < words.size(); j++) {
-                    string& w = words[j];
-                    if (isPal(w, 0, w.size()-1) && j != i) {
-                        ans.push_back(vector<int> {i, j});
-                        ans.push_back(vector<int> {j, i});
+                    if (i != j && isPalindrome(words[j], 0, words[j].length() - 1)) {
+                        ans.push_back({ i, j });
+                        ans.push_back({ j, i });
                     }
                 }
                 continue;
             }
-            string bw = words[i];
-            reverse(bw.begin(), bw.end());
-            if (wmap.find(bw) != wmap.end()) {
-                int res = wmap[bw];
-                if (res != i) ans.push_back(vector<int> {i, res});
+            
+            // 2. Reverse of string is present
+            string rev = words[i];
+            reverse(rev.begin(), rev.end());
+            if (mp.count(rev) && mp[rev] != i) {
+                ans.push_back({ i, mp[rev] });
             }
-            for (int j = 1; j < bw.size(); j++) {
-                if (isPal(bw, 0, j-1)) {
-                    string s = bw.substr(j, bw.size()-j);
-                    if (wmap.find(s) != wmap.end())
-                        ans.push_back(vector<int> {i, wmap[s]});
+            
+            // 3. cut[i+1] is palindrome and reverse cut[0:i] is present
+            // 4. cut[0:i] is palindrome and reverse cut[i+1] is present
+            string str = words[i];
+            for (int j = 1; j < rev.length(); j++) {
+                if (isPalindrome(rev, j, str.length() - 1)) {
+                    string revL = rev.substr(0, j);
+                    if (mp.count(revL)) ans.push_back({ mp[revL], i });
                 }
-                if (isPal(bw, j, bw.size()-1)) {
-                    string s = bw.substr(0, j);
-                    if (wmap.find(s) != wmap.end())
-                        ans.push_back(vector<int> {wmap[s], i});
+                
+                if (isPalindrome(rev, 0, j - 1)) {
+                    string revR = rev.substr(j);
+                    if (mp.count(revR)) ans.push_back({ i, mp[revR] });
                 }
             }
         }
+        
         return ans;
-    }
-    
-private:
-    bool isPal(string& word, int i, int j) {
-        while (i < j)
-            if (word[i++] != word[j--]) return false;
-        return true;
     }
 };
