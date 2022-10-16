946ace3f4299eb8bc787d7a14cbc82daeb5015f9
Prashant Rawat <pr7prashant@gmail.com>
2022-10-16 11:11:50 +0530

    Time: 123 ms (36.31%), Space: 7.9 MB (44.90%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int MOD = 1e9+7;
     
-    int solve(vector<int>& jd, int d, int start, vector<vector<int>>& dp) {
+    int solve(vector<int>& jobDifficulty, int d, int start, vector<vector<int>>& dp) {
         if (d < 0) return MOD;
         
-        if (start == jd.size()) return d == 0 ? 0 : MOD;
+        if (start == jobDifficulty.size()) return d == 0 ? 0 : MOD;
         
         if (dp[start][d] != -1) return dp[start][d];
         
-        int ans = INT_MAX, curr_max = jd[start];
-        for (int i = start; i < jd.size(); i++) {
-            curr_max = max(curr_max, jd[i]);
-            ans = min(ans, curr_max + solve(jd, d-1, i+1, dp));
+        int ans = INT_MAX, curr_max = jobDifficulty[start];
+        for (int i = start; i < jobDifficulty.size(); i++) {
+            curr_max = max(curr_max, jobDifficulty[i]);
+            ans = min(ans, curr_max + solve(jobDifficulty, d-1, i+1, dp));
         }
         
         return dp[start][d] = ans;
