ce4c145585254f2262f0ef82d764ac4841811c90
Prashant Rawat <pr7prashant@gmail.com>
2022-10-25 14:12:37 +0530

    Time: 8 ms (59.56%), Space: 11.3 MB (97.08%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -4,7 +4,6 @@ public:
         int w1 = 0, w2 = 0, i = 0, j = 0;
         while (w1 < word1.size() && w2 < word2.size()) {
             if (word1[w1][i++] != word2[w2][j++]) return false;
-            
             if (i == word1[w1].length()) {
                 w1++;
                 i = 0;
