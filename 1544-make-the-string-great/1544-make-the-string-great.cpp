e62db706a9d2f97eee17d99c21e1232947e22cc6
Prashant Rawat <pr7prashant@gmail.com>
2022-11-08 07:22:12 +0530

    Time: 6 ms (48.12%), Space: 6.9 MB (22.08%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -3,7 +3,6 @@ public:
     string makeGood(string s) {
         int i = 0;
         while (i < s.length() - 1) {
-            //cout << i << " : " << s << endl;
             if (s.length() <= 1) return s;
             
             if (tolower(s[i]) == tolower(s[i+1]) && s[i] != s[i+1]) {
