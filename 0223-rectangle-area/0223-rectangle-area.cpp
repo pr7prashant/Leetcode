d56ff9fafd3a34d1333b35b6c40ab650994efebc
Prashant Rawat <pr7prashant@gmail.com>
2022-11-17 08:30:42 +0530

    Time: 6 ms (92.09%), Space: 6 MB (32.00%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int cx1 = max(ax1, bx1);
+        int cx2 = min(ax2, bx2);
+        int cy1 = max(ay1, by1);
+        int cy2 = min(ay2, by2);
+        int area1 = (ax2 - ax1) * (ay2 - ay1);
+        int area2 = (bx2 - bx1) * (by2 - by1);
+        int area3 = (max(cx2 - cx1, 0)) * (max(cy2 - cy1, 0));
+        return area1 + area2 - area3;
+    }
+};
